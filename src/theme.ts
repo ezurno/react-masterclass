14b154d68cdba75822cbab3b8ff366a27990692a
ezurno <ezurno@gmail.com>
2023-01-18 23:05:06 +0900

    SET: use CSS in styled-components

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   textColor: "#dcdde1",
   bgColor: "#353b48",
-  accentColor: "#44bd32",
+  accentColor: "#9c88ff",
 };

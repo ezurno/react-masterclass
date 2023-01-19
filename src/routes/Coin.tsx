32f70a928dc82bf83daacc4d75f0537f29fe5fa8
ezurno <ezurno@gmail.com>
2023-01-19 19:59:09 +0900

    LEARN: regist interface
    1.
     -browser > console > log value 우클릭 > 전역변수로 object 저장
     - Object.keys(temp).join()
     - Object.values(temp).map(v => typeof v)
    
    2. useState에 generic으로 지정하면 value에서 object를 가져올 수 있음

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -27,13 +27,70 @@ interface RouteParams {
 interface RouteState {
   name: string;
 }
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: "2021-11-10T16:51:15Z";
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
 
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   // info 와 price 값을 받는 object setting
 
   useEffect(() => {
@@ -46,6 +103,8 @@ function Coin() {
       ).json();
 
       console.log(infoData);
+      console.log(priceData);
+
       setInfo(infoData);
       setPriceInfo(priceData);
     })();
